73b94039b74e401fbe9a7b0de0bd0165cfe14f21
Epeaa <zavohirzumaev1@gmail.com>
2024-02-10 22:29:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let element = document.getElementById('element');
+
+function Add() {
+    let task = document.createElement('div');
+    task.style.backgroundColor = 'gray'
+    task.style.width = '300px'
+    task.style.height = '45px'
+    task.style.marginLeft = '20px'
+    task.style.fontSize = '15px'
+    task.style.display = 'flex'
+    task.style.alignItems = 'center'
+    task.style.paddingLeft = '10px'
+    task.style.fontFamily = 'Poppins'
+    task.style.borderRadius = '7px'
+    task.style.marginTop = '10px'
+    task.style.color = 'white'
+
+    let input = document.getElementById('input').value;
+    task.textContent = input;
+    element.appendChild(task);
+
+}
